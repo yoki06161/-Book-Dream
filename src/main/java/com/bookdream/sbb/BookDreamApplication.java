8cf800153a11b164913ffe209cd7993f5e771808
Jaesung <106292056+yoki06161@users.noreply.github.com>
2024-07-03 10:12:00 +0900

    Add files via upload

---
+++ src/main/java/com/bookdream/sbb/BookDreamApplication.java
@@ -0,0 +1,13 @@
+package com.bookdream.sbb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookDreamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookDreamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bookdream/sbb/BookDreamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookdream.sbb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookDreamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
