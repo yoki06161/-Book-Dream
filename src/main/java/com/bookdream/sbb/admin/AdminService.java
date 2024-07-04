ef902a4c4af6f576baf0e4e5306ce2cfd52c4378
hwhwang08 <hwhwang08@gmail.com>
2024-07-04 18:07:55 +0900

    feat:상세보기 성공

--- src/main/java/com/bookdream/sbb/admin/AdminService.java
+++ src/main/java/com/bookdream/sbb/admin/AdminService.java
@@ -1,31 +1,31 @@
-package com.bookdream.sbb.admin;
-
-import java.util.Map;
-import java.util.Optional;
-
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.stereotype.Service;
-
-import com.bookdream.sbb.DataNotFoundException;
-import com.bookdream.sbb.user.*;
-
-import lombok.RequiredArgsConstructor;
-
-@Service
-@RequiredArgsConstructor
-public class AdminService {
-   private final UserRepository userRepository;
-   private final PasswordEncoder passwordEncoder;
-   
-   
-   public SiteUser getAdmin(String username) {
-      Optional<SiteUser> siteUser = this.userRepository.findByusername(username);
-      if(siteUser.isPresent()) {
-         return siteUser.get();
-      }else {
-         throw new DataNotFoundException("siteuser not found!!");
-      }
-   }
-
-
-}
+//package com.bookdream.sbb.admin;
+//
+//import java.util.Map;
+//import java.util.Optional;
+//
+//import org.springframework.security.crypto.password.PasswordEncoder;
+//import org.springframework.stereotype.Service;
+//
+//import com.bookdream.sbb.DataNotFoundException;
+//import com.bookdream.sbb.user.*;
+//
+//import lombok.RequiredArgsConstructor;
+//
+//@Service
+//@RequiredArgsConstructor
+//public class AdminService {
+//   private final UserRepository userRepository;
+//   private final PasswordEncoder passwordEncoder;
+//   
+//   
+//   public SiteUser getAdmin(String username) {
+//      Optional<SiteUser> siteUser = this.userRepository.findByusername(username);
+//      if(siteUser.isPresent()) {
+//         return siteUser.get();
+//      }else {
+//         throw new DataNotFoundException("siteuser not found!!");
+//      }
+//   }
+//
+//
+//}
