6da4ccaf758f4eb894e3bf42114cccce56519250
RubyLia <agfjskf@gmail.com>
2024-07-04 12:43:28 +0900

    Merge branch 'main' of https://github.com/yoki06161/Book-Dream.git into main

--- src/main/java/com/bookdream/sbb/admin/AdminService.java
+++ src/main/java/com/bookdream/sbb/admin/AdminService.java
@@ -1,27 +1,36 @@
-/*
- * package com.bookdream.sbb.admin;
- * 
- * import java.util.Map; import java.util.Optional;
- * 
- * import org.springframework.security.crypto.password.PasswordEncoder; import
- * org.springframework.stereotype.Service;
- * 
- * import com.bookdream.sbb.DataNotFoundException;
- * 
- * import lombok.RequiredArgsConstructor;
- * 
- * @Service
- * 
- * @RequiredArgsConstructor public class AdminService { private final
- * UserRepository userRepository; private final PasswordEncoder passwordEncoder;
- * 
- * public Admin create(Map<String, String> map) { Admin admin = new Admin();
- * admin.setAdminname(map.get("adminname")); admin.setEmail(map.get("email"));
- * admin.setPassword(passwordEncoder.encode(map.get("password")));
- * this.userRepository.save(admin); return admin; }
- * 
- * public Admin getAdmin(String adminname) { Optional<Admin> admin =
- * this.adminRepository.findByadmin(adminname); if(admin.isPresent()) { return
- * Admin()) { } else { throw new DataNotFoundException("admin not found!"); } }
- * }
- */
+package com.bookdream.sbb.admin;
+
+import java.util.Map;
+import java.util.Optional;
+
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.bookdream.sbb.DataNotFoundException;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class AdminService {
+   private final UserRepository userRepository;
+   private final PasswordEncoder passwordEncoder;
+   
+   public Admin create(Map<String, String> map) {
+      Admin admin = new Admin();
+      admin.setAdminname(map.get("adminname"));
+      admin.setEmail(map.get("email"));
+      admin.setPassword(passwordEncoder.encode(map.get("password")));
+      this.userRepository.save(admin);
+      return admin;
+   }
+   
+   public Admin getAdmin(String adminname) {
+      Optional<Admin> admin = this.adminRepository.findByadmin(adminname);
+      if(admin.isPresent()) {
+         return Admin()) {
+      } else {
+         throw new DataNotFoundException("admin not found!");
+      }
+   }
+}
