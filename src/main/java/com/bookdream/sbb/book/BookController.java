4f93adfb1ee3cbd3be915f2261fe79e98b752f82
chinmg <chiming9918@gmail.com>
2024-07-04 13:13:59 +0900

    git

--- src/main/java/com/bookdream/sbb/book/BookController.java
+++ src/main/java/com/bookdream/sbb/book/BookController.java
@@ -1,24 +1,26 @@
 package com.bookdream.sbb.book;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.bookdream.sbb.admin.AdminService;
+import com.bookdream.sbb.admin.Review;
 
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
-@RequestMapping
+@RequestMapping("/book")
 @Controller
 public class BookController {
 	
+	private final ReviewService reviewService;
 	private final BookService bookService;
 	private final AdminService adminService; 
 	
-	@GetMapping("/list")
-	public String list(Model model. @RequestParam(value="page", defaultValue = "0") int page, @RequestParam(value="kw", defaultValue = "") String kw) {
-		
-		Page<Review>
-	}
+	@PreAuthorixe("isAuthenticate()")
+	@PostMapping("/create/{id}")
+	public String create(Model model)
+	
 }
