4626a34f5a9a3e8efeebe61977febc2d73c8312a
chinmg <chiming9918@gmail.com>
2024-07-04 12:46:01 +0900

    git

--- src/main/java/com/bookdream/sbb/admin/AdminCreateForm.java
+++ src/main/java/com/bookdream/sbb/admin/AdminCreateForm.java
@@ -1,5 +1,7 @@
 package com.bookdream.sbb.admin;
 
-public class AdminCreateForm {
-
+public interface AdminCreateForm extends Respoitory<Review, Integer> {
+	Review findReview(String review);
+	Review findReviewAndContent(String review, String content);
+	List<review> findByReviewLike(String review);
 }
