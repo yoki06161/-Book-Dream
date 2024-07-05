8579bafda89c3ee44e843f69be9a206f009788a6
chinmg <chiming9918@gmail.com>
2024-07-05 18:09:38 +0900

    git

--- src/main/java/com/bookdream/sbb/admin/ReviewController.java
+++ src/main/java/com/bookdream/sbb/admin/ReviewController.java
@@ -1,19 +1,40 @@
 package com.bookdream.sbb.admin;
 
+import java.security.Principal;
+
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.bookdream.sbb.user.UserCreateForm;
 import com.bookdream.sbb.user.UserService;
+import com.mysql.cj.x.protobuf.MysqlxCrud.Order;
 
+import ch.qos.logback.core.model.Model;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 @Controller
-@RequestMapping
+@RequestMapping("/answer")
 @RequiredArgsConstructor
 public class ReviewController {
-	private final ReviewService userService;
+	
+	private final ReviewService reviewService;
+	private final AdminService adminService;
+	
+	@PreAuthorize("isAuthenticated()")
+	@PostMapping("/create/{id}")
+	public String create(Model model, @PathVariable("id") Integer id, @Valid ReviewForm reviewForm, BindingResult bindingResult, Principal principal) {
+		Admin admin = adminService.getAdmin();
+		SiteUser siteUser = userService.getUser(principal.getName());
+		if(bindingResult.hasErrors()) {
+			
+		}
+	
 	
 	@GetMapping("/admin")
 	public String sinup() {
