4b5a5d10029f0d24fcf9144af1a97f5a8c52c428
SOMEE-LEE <somee2006@naver.com>
2024-07-05 14:26:34 +0900

    자바파일 수정, 추가 Merge branch 'main' of https://github.com/yoki06161/Book-Dream.git into main

--- src/main/java/com/bookdream/sbb/basket/Basket.java
+++ src/main/java/com/bookdream/sbb/basket/Basket.java
@@ -1,27 +1,27 @@
-//package com.bookdream.sbb.basket;
-//
-//import jakarta.persistence.Column;
-//import jakarta.persistence.Entity;
-//import jakarta.persistence.GeneratedValue;
-//import jakarta.persistence.GenerationType;
-//import jakarta.persistence.Id;
-//import lombok.Getter;
-//import lombok.Setter;
-//
-//@Getter
-//@Setter
-//@Entity
-//public class Basket {
-//	@Id
-//	@GeneratedValue(strategy = GenerationType.IDENTITY)    // auto_increment
-//	private int idx;
-//	
-//	@Column(length = 20)
-//	private String id;
-//	
-//	@Column(columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
-//	private String created_at;
-//	
-//	@Column
-//	private int book_id;
-//}
+package com.bookdream.sbb.basket;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class Basket {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)    // auto_increment
+	private Long idx;
+	
+	@Column(length = 20)
+	private String id;
+	
+	@Column(columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
+	private String created_at;
+	
+	@Column
+	private int book_id;
+}
