200f0321131c646db9888bbde2c1648db6616fd1
SOMEE-LEE <somee2006@naver.com>
2024-07-09 14:32:38 +0900

    feat: 결제 테이블 DB에 추가

--- src/main/java/com/bookdream/sbb/pay/PayRepository.java
+++ src/main/java/com/bookdream/sbb/pay/PayRepository.java
@@ -1,5 +1,7 @@
 package com.bookdream.sbb.pay;
 
-public class PayRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PayRepository  extends JpaRepository<Pay, Long> {
 
 }
