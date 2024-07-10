e09b40cf02ba029c4234d9309cd2ff875bb9c31c
hwhwang08 <hwhwang08@gmail.com>
2024-07-10 17:39:25 +0900

    feat: 패키지 바꿔서 sql넣으니까 됨. 왜?? #25

---
+++ src/main/java/com/bookdream/sbb/prod_repo/review_repository.java
@@ -0,0 +1,8 @@
+package com.bookdream.sbb.prod_repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface review_repository extends JpaRepository<Prod_Review, Integer>{
+
+	
+}
--- src/test/java/com/bookdream/sbb/BookDreamApplicationTests.java
+++ src/test/java/com/bookdream/sbb/BookDreamApplicationTests.java
@@ -1,17 +1,33 @@
 package com.bookdream.sbb;
 
+import java.time.LocalDateTime;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.bookdream.sbb.prod_repo.Prod_Review;
+import com.bookdream.sbb.prod_repo.review_repository;
 
 @SpringBootTest
 class BookDreamApplicationTests {
 
 	@Autowired
-//	private prodrep
+	private review_repository re_repo;
 	
 	@Test
-	void contextLoads() {
+	void testJpa() {
+		Prod_Review q1 = new Prod_Review();
+        q1.setSubject("sbb가 무엇인가요?");
+        q1.setTest("sbb에 대해서 알고 싶습니다.");
+        q1.setTimeIs(LocalDateTime.now());
+        this.re_repo.save(q1);  // 첫번째 질문 저장
+
+        Prod_Review q2 = new Prod_Review();
+        q2.setSubject("sbb라나ㅏㅏㄴ?");
+        q2.setTest("id는 자동생성일까? ");
+        q2.setTimeIs(LocalDateTime.now());
+        this.re_repo.save(q2);  // 첫번째 질문 저장
 	}
 
 }
