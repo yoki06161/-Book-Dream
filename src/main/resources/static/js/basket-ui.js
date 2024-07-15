9c19c9c606038b9a5ac327f48b0e248ced355f71
SOMEE-LEE <somee2006@naver.com>
2024-07-15 17:45:43 +0900

    feat:장바구니에서 삭제 버튼 클릭 시 해당 항목 삭제
    모달창 없음+sessionBookIds 삭제/뱃지숫자 반영/삭제직후 상품없다고 뜸

--- src/main/resources/static/js/basket-ui.js
+++ src/main/resources/static/js/basket-ui.js
@@ -44,87 +44,128 @@ function init() {
 
     // 페이지 로드 후 세션 스토리지에 저장된 formData 출력
     displayFormData();
-	// 장바구니 페이지에서 수량 변경 시 호출
-	//updateCountPrice();
 }
 
 // 세션 스토리지에서 폼데이터를 가져와서 콘솔에 출력하는 함수
 function displayFormData() {
-    // 세션 스토리지에서 'book_ids' 배열을 불러오기 (없으면 빈 배열로 초기화)
-    let sessionBookIds = JSON.parse(sessionStorage.getItem('book_ids')) || [];
-        
     // 세션 스토리지에서 장바구니 내역 배열을 불러오기 (없으면 빈 배열로 초기화)
-    let dataArray = JSON.parse(sessionStorage.getItem("dataArray")) || [];
+	let dataArray = JSON.parse(sessionStorage.getItem("dataArray")) || [];
     let dataArrayList = document.getElementById('dataArrayList');
 	let dataNotFound = document.getElementById('dataNotFound');
-    
-    
-    if (sessionBookIds.length > 0 && dataArray.length > 0) {
-        dataArray.forEach(function(data,index) {
-			// tr요소 생성 변수 
-			let tr = document.createElement('tr');
-            
-            // select 요소 만들고, 선택된 옵션을 제외한 다른 옵션을 고를 수 있게 하기
-            let selectElement = document.createElement('select');
-			selectElement.className = `count${index}`; // 고유 클래스 이름 추가
-            for (let i = 1; i <= 10; i++) {
-                let option = document.createElement('option');
-                option.value = i;
-                option.textContent = i;
-                if (i == data.count) {
-                    option.selected = true; // 선택된 select 값
-                }
-                selectElement.appendChild(option);
-            }
-			
-			// Select 요소 값이 변경될 때마다 값을 업데이트하는 이벤트 리스너 추가
-			selectElement.addEventListener('change', function() {
-				let count = this.value;
-				console.log('Selected count:', count, 'for element:', selectElement.className);
+	console.log(dataArrayList);
+	    
+	if (dataArray.length > 0) {
+	    dataArray.forEach(function(data, index) {
+	        // tr 요소 생성 변수
+	        let tr = document.createElement('tr');
 
-				// ,제거 후 실수로 변환. ,가 찾을 문자. g는 전역 검색을 뜻. 모든 ','을 ''로 바꾸겠단 뜻
-				let price = document.querySelector(`.row${index} .price`).textContent;
-				console.log(price);
+	        // select 요소 만들고, 선택된 옵션을 제외한 다른 옵션을 고를 수 있게 하기
+	        let selectElement = document.createElement('select');
+	        selectElement.className = `count${index}`; // 고유 클래스 이름 추가
+	        for (let i = 1; i <= 10; i++) {
+	            let option = document.createElement('option');
+	            option.value = i;
+	            option.textContent = i;
+	            if (i == data.count) {
+	                option.selected = true; // 선택된 select 값
+	            }
+	            selectElement.appendChild(option);
+	        }
 
-				// , 제거 후 실수로 변환
-				price = parseFloat(price.replace(/,/g, ''));
-				console.log(price);
-				                        
-				let result = document.querySelector(`.row${index} .result`);
+	        // Select 요소 값이 변경될 때마다 값을 업데이트하는 이벤트 리스너 추가
+	        selectElement.addEventListener('change', function() {
+	            updateTotalPrice(index);
+	        });
 
-				if (count > 0) {
-					let total = count * price;
-				    result.textContent = total.toLocaleString() + '원';
-				} 
-			});
+	        let btnClose = document.createElement('button');
+	        btnClose.type = 'button';
+	        btnClose.className = 'btn-close';
+	        btnClose.setAttribute('aria-label', 'Close');
 
-            tr.innerHTML = `
-				<input type="hidden" value=${data.book_id}>
-                <td><input class="form-check-input" type="checkbox"></td>
-                <td><img src=${data.book_img} alt="상품사진" style="width: 82px; height: 118.34px;"></td>
-                <td class="text-start"><span class="fs-5 fw-bold">${data.book_title}</span>
-				<br><p class="price">${data.book_price}</p></td>
-				<td></td>
-                <td class="result">${data.count_price}</td>
-                <td><button type="button" class="btn-close" aria-label="Close"></button></td>`;
-            
-            // select 요소 td에 넣기
-            tr.children[4].appendChild(selectElement);
-			tr.children[4].appendChild(document.createTextNode(' 권')); // "권" 텍스트 추가
-			
-			// 각 행에 고유한 클래스를 추가합니다.
-			tr.className = `row${index}`;
-            dataArrayList.appendChild(tr);
-        });
+	        // btnClose 클릭 이벤트 리스너
+	        btnClose.addEventListener('click', function() {
 
-        // '전체선택' 체크박스 상태와 주문 버튼 상태 업데이트
-        initCheckboxes();
-    } else {
-		// tr요소 생성 변수 
-		let div = document.createElement('div');
-		div.innerHTML = `<h3 class="text-center">장바구니에 담은 상품이 없습니다</h3>`;
-        dataNotFound.append(div);
-    }
+	            let deleteNum = document.querySelector(`.row${index} input[type="hidden"]`).value;
+				
+	            // 삭제할 book_id
+	            let bookIdToDelete = data.book_id;
+
+	            // 배열에서 해당 book_id를 가진 요소의 인덱스 찾기
+	            let indexToDelete = dataArray.findIndex(item => item.book_id === bookIdToDelete);
+
+				// 배열에서 해당 인덱스의 요소 삭제
+				if (indexToDelete !== -1) {
+					// 배열에서 요소 삭제
+				    dataArray.splice(indexToDelete, 1);
+					
+				    // sessionStorage에 업데이트된 배열 저장
+				    sessionStorage.setItem("dataArray", JSON.stringify(dataArray));
+
+				    // 콘솔 확인
+				    console.log('Updated dataArray:', dataArray);
+					
+					// 장바구니 배열 길이 가져오기
+					dataArray = JSON.parse(sessionStorage.getItem("dataArray")) || [];
+					let badgeCount = dataArray.length;
+
+					// sessionStorage에 badgeCount 저장
+					sessionStorage.setItem('badgeCount', badgeCount);
+
+					// 뱃지 숫자 설정
+					document.getElementById('badge').textContent = badgeCount;
+				} else {
+				    console.log(`Book ID ${bookIdToDelete} not found in dataArray`);
+				}
+	            // 해당 행을 삭제
+	            tr.remove(bookIdToDelete);
+	        });
+
+	        tr.innerHTML = `
+	            <input type="hidden" value=${data.book_id}>
+	            <td><input class="form-check-input" type="checkbox"></td>
+	            <td><img src=${data.book_img} alt="상품사진" style="width: 82px; height: 118.34px;"></td>
+	            <td class="text-start"><span class="fs-5 fw-bold">${data.book_title}</span>
+	            <br><p class="price">${data.book_price}</p></td>
+	            <td></td>
+	            <td class="result">${data.count_price}</td>
+	            <td></td>`;
+
+	        // select 요소 td에 넣기
+			tr.children[4].appendChild(selectElement);
+			tr.children[4].appendChild(document.createTextNode(' 권')); 
+
+	        // btnClose 요소 td에 넣기
+	        let tdClose = document.createElement('td');
+	        tr.children[6].appendChild(btnClose);
+
+	        // 각 행에 고유한 클래스를 추가합니다.
+	        tr.className = `row${index}`;
+	        dataArrayList.appendChild(tr);
+	    });
+
+	    // '전체선택' 체크박스 상태와 주문 버튼 상태 업데이트
+	    initCheckboxes();
+	} else {
+	    // div 요소 생성 변수
+	    let div = document.createElement('div');
+	    div.innerHTML = `<h3 class="text-center">장바구니에 담은 상품이 없습니다</h3>`;
+	    dataNotFound.append(div);
+	}
+
+	function updateTotalPrice(index) {
+	    let count = document.querySelector(`.row${index} select`).value;
+	    let price = document.querySelector(`.row${index} .price`).textContent;
+
+	    // ,제거 후 실수로 변환. ,가 찾을 문자. g는 전역 검색을 뜻. 모든 ','을 ''로 바꾸겠단 뜻
+	    price = parseFloat(price.replace(/,/g, ''));
+
+	    let result = document.querySelector(`.row${index} .result`);
+
+	    if (count > 0) {
+	        let total = count * price;
+	        result.textContent = total.toLocaleString() + '원';
+	    }
+	}
 }
 
 function initCheckboxes() {
--- src/main/resources/static/js/common-ui.js
+++ src/main/resources/static/js/common-ui.js
@@ -2,10 +2,18 @@ function init() {
 	// 세션 스토리지 전체 삭제(저장이 잘못되었을때 주석풀고 사용)
 	//sessionStorage.clear();
 	
-	// sessionStorage에서 저장된 뱃지 숫자를 가져와서 설정
-	if (sessionStorage.getItem('badgeCount')) {
-		document.getElementById('badge').textContent = sessionStorage.getItem('badgeCount');
-	}
+	// 세션 스토리지에서 장바구니 내역 배열을 불러오기 (없으면 빈 배열로 초기화)
+	let dataArray = JSON.parse(sessionStorage.getItem("dataArray")) || [];
+	console.log(dataArray);
+	
+	// 장바구니 배열 길이 가져오기
+	let badgeCount = dataArray.length;
+
+	// sessionStorage에 badgeCount 저장
+	sessionStorage.setItem('badgeCount', badgeCount);
+
+	// 뱃지 숫자 설정
+	document.getElementById('badge').textContent = badgeCount;
 }
 
 // DOMContentLoaded 이벤트 리스너를 사용하여 문서가 완전히 로드된 후 실행하는 함수 호출
@@ -21,9 +29,6 @@ function aa() {
 	let count = parseInt(document.querySelector('.bcount').value);     // int형으로 담기
 	let count_price = document.querySelector('.result').textContent + '원';
 	//console.log(book_id);
-
-	// 세션 스토리지에서 'book_ids' 배열을 불러오기 (없으면 빈 배열로 초 기화)
-	let sessionBookIds = JSON.parse(sessionStorage.getItem('book_ids')) || [];
 	
 	// 세션 스토리지에 저장하기 위해 객체로 묶어 JSON 문자열로 변환
 	let dataToStore = {
@@ -34,23 +39,18 @@ function aa() {
 	    count: count,
 	    count_price: count_price
 	};
-
+	
 	// 세션 스토리지에서 장바구니 내역 배열을 불러오기 (없으면 빈 배열로 초기화)
 	let dataArray = JSON.parse(sessionStorage.getItem("dataArray")) || [];
-	//let dataArray = [];
 	console.log(dataArray);
 	
 	
 	// book_id가 세션 스토리지에 없는 경우 book_id와  장바구니에 상품 정보를 각각 배열에 추가하고 뱃지 숫자 업데이트
-	if (!sessionBookIds.includes(book_id)) {
-		// book_id를 배열에 추가
-	    sessionBookIds.push(book_id);
+	if (!dataArray.some(item => item.book_id === book_id)) {
 		// 새 객체를 배열에 추가
 		dataArray.push(dataToStore);
 		// 배열을 JSON 문자열로 변환하여 세션 스토리지에 저장
-	    sessionStorage.setItem('book_ids', JSON.stringify(sessionBookIds)); 
 		sessionStorage.setItem("dataArray", JSON.stringify(dataArray));
-		console.log(sessionBookIds);
 		console.log(dataArray);
 		alert('장바구니에 추가되었습니다.');
 
