bda2b9fd11a788b53a22264d59a6e05bb45a2eaa
yoki06161 <yoki06161@naver.com>
2024-07-16 10:05:08 +0900

    feat: 상세보기 수정 #1

--- src/main/java/com/bookdream/sbb/MainController.java
+++ src/main/java/com/bookdream/sbb/MainController.java
@@ -1,14 +1,14 @@
-//package com.bookdream.sbb;
-//
-//import org.springframework.stereotype.Controller;
-//import org.springframework.web.bind.annotation.GetMapping;
-//
-//@Controller
-//public class MainController {
-//
-//	@GetMapping("/")	
-//	public String index() {
-//		
-//		return "layout";
-//	}
-//}
+package com.bookdream.sbb;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+	@GetMapping("/")	
+	public String index() {
+		
+		return "layout";
+	}
+}
