92275d3460081741d06c3129042e75c41c32ad98
yoki06161 <yoki06161@naver.com>
2024-07-17 11:07:16 +0900

    feat: 채팅 시간 표시 수정 #1

--- src/main/java/com/bookdream/sbb/trade/chat/ChatRoom.java
+++ src/main/java/com/bookdream/sbb/trade/chat/ChatRoom.java
@@ -61,6 +61,10 @@ public class ChatRoom {
             return newMessagesCountForSender;
         }
     }
+    
+    public String getFormattedLastMessageTime() {
+        return DateUtils.formatDateTime(lastMessageTime);
+    }
 }
 
 
