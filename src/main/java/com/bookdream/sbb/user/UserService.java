340e5c76f3b0ac0e57c79812942fba6e68c80883
hwhwang08 <hwhwang08@gmail.com>
2024-07-18 11:37:32 +0900

    feat: 두목님꺼 합쳤으나 db에 내 책이 새로 만들어짐 #63

--- src/main/java/com/bookdream/sbb/user/UserService.java
+++ src/main/java/com/bookdream/sbb/user/UserService.java
@@ -1,14 +1,14 @@
 package com.bookdream.sbb.user;
 
+import java.time.LocalDateTime;
 import java.util.Map;
 import java.util.Optional;
 
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import com.bookdream.sbb.DataNotFoundException;
-import org.springframework.dao.DataIntegrityViolationException;
 
 import lombok.RequiredArgsConstructor;
 
@@ -17,16 +17,11 @@ import lombok.RequiredArgsConstructor;
 public class UserService {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
-
+    
     public SiteUser create(Map<String, String> map) {
         // 중복 이메일 검사
         if (userRepository.existsByEmail(map.get("email"))) {
             throw new DataIntegrityViolationException("이미 등록된 이메일입니다.");
-        }
-
-        // 중복 사용자 이름 검사
-        if (userRepository.existsByUsername(map.get("username"))) {
-            throw new DataIntegrityViolationException("이미 등록된 사용자 이름입니다.");
         }
 
         SiteUser user = new SiteUser();
@@ -37,6 +32,7 @@ public class UserService {
         return user;
     }
 
+    
     public SiteUser getUser(String email) {
         Optional<SiteUser> siteUser = this.userRepository.findByEmail(email);
         if (siteUser.isPresent()) {
@@ -46,15 +42,7 @@ public class UserService {
         }
     }
 
-    @Transactional
-    public SiteUser createKakaoUser(String email, String nickname) {
-        SiteUser user = new SiteUser();
-        user.setEmail(email);
-        user.setUsername(nickname);
-        user.setPassword(""); // 카카오 로그인 사용자는 비밀번호가 없을 수 있음
-        System.out.println("Saving new user: " + user);
-        return userRepository.save(user);
-    }
+
 
     public SiteUser getUserByEmail(String email) {
     	Optional<SiteUser> siteUser = this.userRepository.findByEmail(email);
@@ -64,4 +52,44 @@ public class UserService {
             throw new DataNotFoundException("siteuser not found!!");
         }
     }
+    
+    public boolean checkPassword(String email, String password) {
+        Optional<SiteUser> siteUser = userRepository.findByEmail(email);
+
+        // 사용자가 존재하지 않는 경우
+        if (!siteUser.isPresent()) {
+            return false; // 해당 이메일을 가진 사용자가 없음
+        }
+
+        // 데이터베이스에서 조회한 사용자 객체
+        SiteUser user = siteUser.get();
+
+        // 입력받은 비밀번호와 데이터베이스의 비밀번호 비교
+        return user.getPassword().equals(password);
+    }
+    
+    // 사용자 업데이트 메서드
+    public void modifyPassword(SiteUser user, String password) {
+    	user.setPassword(passwordEncoder.encode(password));
+        this.userRepository.save(user);
+    }
+    
+    public void modifyName(SiteUser user, String name) {
+        user.setUsername(name);
+        user.setLastNameChangeDate(LocalDateTime.now()); // 이름 변경 일시 설정
+        this.userRepository.save(user);
+    }
+
+
+    public boolean isSamePassword(SiteUser user, String password){
+        return passwordEncoder.matches(password, user.getPassword());
+    }
+    
+    // 사용자 삭제 메서드
+    public void deleteUser(SiteUser user) {
+        this.userRepository.delete(user);
+    }
+    
+
+    
 }
