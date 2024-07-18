4a55469c4fd71a9a8065dd53ab490f73b32a461d
hwhwang08 <hwhwang08@gmail.com>
2024-07-18 18:10:38 +0900

    feat: 테이블 명 소문자로 #105

--- src/main/java/com/bookdream/sbb/basket/Basket.java
+++ src/main/java/com/bookdream/sbb/basket/Basket.java
@@ -5,9 +5,11 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
 
+@Table (name = "basket")
 @Getter
 @Setter
 @Entity
