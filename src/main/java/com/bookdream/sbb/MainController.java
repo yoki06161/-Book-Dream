5341e9321df52767ef4273d261ae3d151e8fb763
hwhwang08 <hwhwang08@gmail.com>
2024-07-18 10:55:48 +0900

    feat: 두목님꺼하기전 푸시 #62

--- src/main/java/com/bookdream/sbb/MainController.java
+++ src/main/java/com/bookdream/sbb/MainController.java
@@ -1,25 +1,15 @@
 package com.bookdream.sbb;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 
-import com.bookdream.sbb.basket.BasketService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
-	
-	@Autowired
-	private BasketService basketService;
 
 	@GetMapping("/")	
-	public String index(Model model, String id) {
-//		Long count = basketService.getCountBasketsById(id);
-//		
-//		count = (count != null) ? count : 0L; // null 체크 및 0으로 치환
-//		
-//	    model.addAttribute("count", count);
+	public String index() {
+		
 		return "layout";
 	}
 }
