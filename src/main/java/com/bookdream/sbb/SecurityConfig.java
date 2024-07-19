9b5c9c5802e8a858febe81fa2a1e94d125132a3c
hwhwang08 <hwhwang08@gmail.com>
2024-07-19 11:09:11 +0900

    feat: 로그인과 완전 합체. 카카오 로그인 조금 수정 필요 #107

--- src/main/java/com/bookdream/sbb/SecurityConfig.java
+++ src/main/java/com/bookdream/sbb/SecurityConfig.java
@@ -36,7 +36,7 @@ public class SecurityConfig {
                 .addHeaderWriter(new XFrameOptionsHeaderWriter(
                     XFrameOptionsHeaderWriter.XFrameOptionsMode.SAMEORIGIN)))
             .formLogin((formLogin) -> formLogin
-                .loginPage("/user/loginform")
+                .loginPage("/user/login")
                 .loginProcessingUrl("/user/login") 
                 .failureUrl("/user/login?error=true")
                 .defaultSuccessUrl("/"))
