ee19d3bf899eab9be725962cf74d72820b1caee7
SOMEE-LEE <somee2006@naver.com>
2024-07-22 17:32:20 +0900

    refact: 결제는 되는데 로그아웃은 안되는버전(진짜로)

--- src/main/java/com/bookdream/sbb/pay/Pay.java
+++ src/main/java/com/bookdream/sbb/pay/Pay.java
@@ -7,9 +7,11 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
 
+@Table (name = "pay")
 @Getter
 @Setter
 @Entity
