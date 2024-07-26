e818435676796d88589476486591a7fffe8650ac
RubyLia <agfjskf@gmail.com>
2024-07-26 17:08:34 +0900

    feat: css 수정(간격, 타이틀 등)

--- src/main/java/com/bookdream/sbb/user/CustomOauth2UserService.java
+++ src/main/java/com/bookdream/sbb/user/CustomOauth2UserService.java
@@ -31,6 +31,9 @@ public class CustomOauth2UserService extends DefaultOAuth2UserService {
         } else if (provider.equals("kakao")) {
             log.info("카카오 로그인");
             oAuth2UserInfo = new KakaoUserDetails(oAuth2User.getAttributes());
+        } else if (provider.equals("naver")) {
+            log.info("네이버 로그인");
+            oAuth2UserInfo = new NaverUserDetails(oAuth2User.getAttributes());
         }
 
         String providerId = oAuth2UserInfo.getProviderId();
