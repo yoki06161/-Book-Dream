7e377d1422a95114a5261ddcf1bdaa3f5ef77a06
SOMEE-LEE <somee2006@naver.com>
2024-07-26 14:50:04 +0900

    fix: 장바구니/바로구매 버그 수정, style: 헤더 레이아웃 수정
    Merge branch 'main' of https://github.com/yoki06161/Book-Dream.git into
    main

--- src/main/java/com/bookdream/sbb/admin/AdminController.java
+++ src/main/java/com/bookdream/sbb/admin/AdminController.java
@@ -25,9 +25,4 @@ public class AdminController {
    public String signup() {
       return "/login";
    }
-<<<<<<< HEAD
 }
-
-=======
-}
->>>>>>> branch 'main' of https://github.com/yoki06161/Book-Dream.git
--- src/main/java/com/bookdream/sbb/basket/BasketService.java
+++ src/main/java/com/bookdream/sbb/basket/BasketService.java
@@ -15,14 +15,27 @@ public class BasketService {
     private BasketRepository basketRepository;
 
     @Transactional
-    public void saveBasketItems(List<Map<String, Object>> sessionData) {
-        for (Map<String, Object> data : sessionData) {
-            Basket basket = new Basket();
-            basket.setEmail((String) data.get("email"));
-            basket.setBook_id((Integer) data.get("book_id"));
-            basket.setCount((Integer) data.get("count"));
-            basket.setCount_price((String) data.get("count_price"));
-            basketRepository.save(basket);
+    public void saveBasketItems(List<Map<String, Object>> sessionData, String email) {
+        List<Basket> basketsToSave = sessionData.stream()
+                .map(data -> {
+                    Integer bookId = Integer.valueOf(data.get("book_id").toString());
+                    Optional<Basket> existingBasket = basketRepository.findByBookIdAndEmail(bookId, email);
+                    if (existingBasket.isEmpty()) {
+                        Basket basket = new Basket();
+                        basket.setEmail(email);
+                        basket.setBook_id(bookId);
+                        basket.setCount(Integer.valueOf(data.get("count").toString()));
+                        basket.setCount_price(data.get("count_price").toString());
+                        return basket;
+                    } else {
+                        return null; // 중복된 항목은 제외
+                    }
+                })
+                .filter(basket -> basket != null) // null 값을 제외
+                .collect(Collectors.toList());
+
+        if (!basketsToSave.isEmpty()) {
+            basketRepository.saveAll(basketsToSave);
         }
     }
     
